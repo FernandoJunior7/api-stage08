990caf2f9124f1745f890dad3e4433225f8f6987
FernandoJunior7 <fjr7@Fernandos-MacBook-Pro.local>
2024-01-25 15:26:03 -0300

    add: knex dependency

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,5 @@
+const { hash, compare} = require('bcryptjs');
+
 const AppError = require('../utils/AppError');
 
 const sqliteConnection = require('../database/sqlite');
@@ -14,9 +16,55 @@ class UsersController {
       throw new AppError('Este e-mail já está em uso');
     }
 
-    await database.run('INSERT INTO users (name, email, password) VALUES ((?), (?), (?))', [name, email, password]);
+    const hashedPassword = await hash(password, 8);
+
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
 
     return response.status(201).json();
+  }
+
+  async update(request, responnse) {
+    const {name, email, password, old_password } = request.body;
+    const {id} = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Este email já está em uso');
+    }
+
+    user.name = name ?? user.name;
+    user.mail = mail ?? user.mail;
+
+    if (password && !old_password) {
+      throw new AppError('Você precisa informar sua senha atual para poder definir uma nova senha');
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha não confere');
+      }
+
+      user.password = await has(password, 8);
+    }
+
+    await databse.run(`
+      UPDATE users SET
+      name = ?,
+      updated_at = DATETIME('NOW'),
+      WHERE id = ?`, [user.name, user.mail, user.password, id]
+    );
+    
+    return response.status(200).json();
   }
 }
 
