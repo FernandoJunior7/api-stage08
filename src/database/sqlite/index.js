8364326c355028e2f53e07871e360c9e63dabe84
FernandoJunior7 <fjr7@Fernandos-MacBook-Pro.local>
2024-01-26 13:14:06 -0300

    now the databases is fully working

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -2,6 +2,8 @@ const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 const path = require('path');
 
+// __dirname é o nome do diretório atual
+
 async function sqliteConnection() {
   const database = await sqlite.open({
     filename: path.resolve(__dirname, '..', 'database.db'),
